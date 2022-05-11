d347096c72ca0a5a99c729bdfa37893d4c34e77b
Gaurab Kumar <progaurab@gmail.com>
2022-05-11 21:40:53 +0530

    virtual dom

---
+++ script.js
@@ -0,0 +1,22 @@
+const jsDiv = document.getElementById("dateJs");
+const reactDiv = document.getElementById("dateReact");
+
+const render = () => {
+  jsDiv.innerHTML = `
+    <div class="split">
+      JS template
+      <p>${new Date()}</p>
+    </div>
+  `;
+
+  var divToRender = React.createElement(
+    "div",
+    { className: "split" },
+    "React template ",
+    React.createElement("p", null, new Date().toString())
+  );
+
+  ReactDOM.render(divToRender, reactDiv);
+};
+
+setInterval(render, 1000);
